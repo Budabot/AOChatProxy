6243b9d7235d390f903e046a0ed48dc9ae66d623
Tyrence <email1@jkbff.com>
2014-05-02 00:54:10 -0600

    updated log4j to use xml file

--- src/main/scala/com/jkbff/ao/chatproxy/Program.scala
+++ src/main/scala/com/jkbff/ao/chatproxy/Program.scala
@@ -16,91 +16,91 @@ import com.jkbff.ao.tyrlib.packets.BaseClientPacket
 import com.jkbff.ao.tyrlib.chat.MMDBParser
 
 object Program {
-  private var bots:HashMap[String, AOSocket] = null
+	private var bots: HashMap[String, AOSocket] = null
 
-  private val log = getLogger("com.jkbff.ao.chatproxy.Program")
+	private val log = getLogger("com.jkbff.ao.chatproxy.Program")
 
-  def main(args: Array[String]) {
-    Program.run()
-  }
+	def main(args: Array[String]) {
+		Program.run()
+	}
 
-  def run(): Unit = {
-    // tell the lib where to find the text.mdb file
-    MMDBParser.fileLocation = "text.mdb";
+	def run(): Unit = {
+		// tell the lib where to find the text.mdb file
+		MMDBParser.fileLocation = "text.mdb";
 
-    // initialize the log4j component
-    PropertyConfigurator.configure("log4j.properties");
+		// initialize the log4j component
+		PropertyConfigurator.configure("log4j.xml");
 
-    // load the properties file for the bot info
-    val properties = new Properties();
-    properties.load(new FileInputStream("chatbot.properties"));
+		// load the properties file for the bot info
+		val properties = new Properties();
+		properties.load(new FileInputStream("chatbot.properties"));
 
-    val serverSocket = new ServerSocket(properties.getProperty("proxyPortNumber").toInt)
+		val serverSocket = new ServerSocket(properties.getProperty("proxyPortNumber").toInt)
 
-    var socket: Socket = null
-    while (true) {
-      try {
-        log.info("Ready to accept a master bot on port " + properties.getProperty("proxyPortNumber"))
+		var socket: Socket = null
+		while (true) {
+			try {
+				log.info("Ready to accept a master bot on port " + properties.getProperty("proxyPortNumber"))
 
-        socket = serverSocket.accept
+				socket = serverSocket.accept
 
-        val clientHandler = new ClientHandler
-        clientHandler.mainBotCharacter = properties.getProperty("bot1_characterName")
+				val clientHandler = new ClientHandler
+				clientHandler.mainBotCharacter = properties.getProperty("bot1_characterName")
 
-        bots = createBots(properties, clientHandler);
+				bots = createBots(properties, clientHandler);
 
-        clientHandler.socket = socket
-        clientHandler.bots = bots;
-        clientHandler.setName("clientHandler")
-        clientHandler.start
+				clientHandler.socket = socket
+				clientHandler.bots = bots;
+				clientHandler.setName("clientHandler")
+				clientHandler.start
 
-        // check to make sure everything is running
-        do {
-          Thread.sleep(5000)
-        } while (clientHandler.isRunning)
-      } catch {
-        case e: Exception => {
-          socket.close
-          log.error("master bot connection ended", e)
-        }
-      }
+				// check to make sure everything is running
+				do {
+					Thread.sleep(5000)
+				} while (clientHandler.isRunning)
+			} catch {
+				case e: Exception => {
+					socket.close
+					log.error("master bot connection ended", e)
+				}
+			}
 
-      shutdownBots
-      socket.close
-    }
-  }
+			shutdownBots
+			socket.close
+		}
+	}
 
-  def createBots(properties: Properties, clientHandler: ClientHandler): HashMap[String, AOSocket] = {
-    var count = 1
-    val bots = new HashMap[String, AOSocket];
+	def createBots(properties: Properties, clientHandler: ClientHandler): HashMap[String, AOSocket] = {
+		var count = 1
+		val bots = new HashMap[String, AOSocket];
 
-    while (properties.getProperty("bot" + count + "_characterName") != null) {
-      val botName = "bot" + count + "_";
+		while (properties.getProperty("bot" + count + "_characterName") != null) {
+			val botName = "bot" + count + "_";
 
-      // create the connection
-      val conn = new AOSocket(new AOSocketInfo(
-        properties.getProperty(botName + "username"),
-        properties.getProperty(botName + "password"),
-        properties.getProperty(botName + "characterName"),
-        properties.getProperty("serverAddress"),
-        Integer.parseInt(properties.getProperty("serverPortNumber"))), clientHandler);
+			// create the connection
+			val conn = new AOSocket(new AOSocketInfo(
+				properties.getProperty(botName + "username"),
+				properties.getProperty(botName + "password"),
+				properties.getProperty(botName + "characterName"),
+				properties.getProperty("serverAddress"),
+				Integer.parseInt(properties.getProperty("serverPortNumber"))), clientHandler);
 
-      // set the login info
-      val id = if (count != 1) "slave" + (count - 1) else "main"
-      conn.setName(id);
-      bots.put(id, conn);
+			// set the login info
+			val id = if (count != 1) "slave" + (count - 1) else "main"
+			conn.setName(id);
+			bots.put(id, conn);
 
-      log.debug("created proxy bot " + properties.getProperty(botName + "characterName"))
+			log.debug("created proxy bot " + properties.getProperty(botName + "characterName"))
 
-      count += 1
-    }
-    bots
-  }
+			count += 1
+		}
+		bots
+	}
 
-  def shutdownBots(): Unit = {
-    // shutdown any currently running bots
-    if (bots != null) {
-      bots.values.par.foreach(bot => bot.shutdown)
-    }
-  }
+	def shutdownBots(): Unit = {
+		// shutdown any currently running bots
+		if (bots != null) {
+			bots.values.par.foreach(bot => bot.shutdown)
+		}
+	}
 }
